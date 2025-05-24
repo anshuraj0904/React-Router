790a564afa006a33e5524ca114578257ace628af
anshuraj0904 <rajanshu0904@gmail.com>
2025-05-25 05:29:32 +0530

    Final Push

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+import { useLoaderData } from 'react-router-dom' // useLoaderData is used to access data loaded by the route loader function.
 
 function Github() {
+    const data = useLoaderData() // This will give you the data returned by the githubInfoLoader function.
+
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/anshuraj0904')
+    //     .then((res)=> res.json())
+    //     .then((data)=>{
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    //     .catch((err)=> console.log(err))
+    // }, [])
+
+    // Note:- We'll not use the classic useState and useEffect here. Instead, here we'll use the data fetched by the loader function directly in the component. This is a more efficient way to handle data fetching in React Router.
+    // For this, we need useLoaderData hook from react-router-dom, which allows us to access the data returned by the loader function defined in the route configuration. 
+    // And, we've set loader=githubInfoLoader in the route configuration for the Github component, which fetches the data from the GitHub API before rendering the component.
   return (
-    <div>Github</div>
+    <div className='text-center m-4 bg-gray-400 text-white p-4 text-3xl'>
+      <h1>Username: {data.name}</h1>
+    </div>
   )
 }
 
 export default Github
+
+export const githubInfoLoader = async()=>{
+   const respone = await   fetch('https://api.github.com/users/anshuraj0904')
+   return respone.json()
+} 
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import Layout from './layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
-// import {Github, githubInfoLoader} from './components/Github/Github.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 import User from './components/User/User.jsx'
 
 const router = createBrowserRouter(
@@ -17,8 +17,12 @@ const router = createBrowserRouter(
       <Route path='user/' element={<User />} >
          <Route path=':userid' element={<User />} /> {/* dynamic route for user ID */}
       </Route>
-      {/* <Route loader={githubInfoLoader} path='github' element={<Github />} />  */}
       {/* This automatically has the '/' prefix because it's a child of the root path. */}
+      <Route 
+      loader={githubInfoLoader} // This loader function fetches data before the component is rendered.
+      path='github'
+      element={<Github/>} />
+      
       <Route path='*' element={<h1>404 Not Found</h1>} />
     </Route>
   )
